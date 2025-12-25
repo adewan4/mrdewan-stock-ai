714a5dd658c144bb02c3025c6668305eefbaefc0
adewan4 <akash.dewan5@gmail.com>
2025-12-25 14:43:08 +0530

    app redeployed

--- app.py
+++ app.py
@@ -1,4 +1,2 @@
 import streamlit as st
-import pandas as pd
-import os
-from ai_engine import fetch_basic_info, calculate_scores_from_info, get_news_balance_cashflow_financials
+st.write("App is running clean")
