336f2424fba4e66ad55513179c30182a4bcd6ad4
adewan4 <akash.dewan5@gmail.com>
2025-12-25 14:35:56 +0530

    Hard reset app.py to clean ASCII file

--- app.py
+++ app.py
@@ -1,2 +1,4 @@
-raise Exception("This is the real app file")
-
+import streamlit as st
+import pandas as pd
+import os
+from ai_engine import fetch_basic_info, calculate_scores_from_info, get_news_balance_cashflow_financials
