d4028343e289eabc0821a801efd701d21c272566
adewan4 <akash.dewan5@gmail.com>
2025-12-28 18:18:58 +0530

    New update

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pandas as pd
 import yfinance as yf
-import os
+import time
 from ai_engine import (fetch_basic_info, calculate_scores_from_info, intraday_signal,)
 
 # -------------------------------------------------
